6b27ef1f98ba418bf5985cbbd18d663dc5110d5a
agrani Kumar <158788185+Agrani-kumar-01@users.noreply.github.com>
2024-03-18 16:57:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const express = require("express")
+const app = express()
 
+// require("./routes/idea.routes")(app)
+const routers = require("./routes/idea.routes")
+routers(app)
+
+app.listen(8000,()=>{
+    console.log("App started on the port no : ",8000);
+})
