f51aa417e2eebbfb4113e5c578757c19487c7f61
agrani Kumar <158788185+Agrani-kumar-01@users.noreply.github.com>
2024-03-18 16:53:33 +0530

    Update idea.controller.js

--- controllers/idea.controller.js
+++ controllers/idea.controller.js
@@ -1,1 +1,16 @@
+// Write the logic for crreating controller for the idea resource
 
+const ideas = require("../models/ideas.model")
+
+exports.getAllIdeas = (req,res)=>{
+    // first read all the ideas from the idea model file
+    // return all those ideas
+    let std_id = req.query.id
+    let std_obj = ideas[std_id]
+    if(std_obj){
+        res.status(200).send(std_obj)
+    }else{
+        res.status(500).send(ideas)
+    }
+
+}
