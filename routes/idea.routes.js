99ac07181a9a5165226d999e19afc60f989e8a41
agrani Kumar <158788185+Agrani-kumar-01@users.noreply.github.com>
2024-03-18 16:56:32 +0530

    Update idea.routes.js

--- routes/idea.routes.js
+++ routes/idea.routes.js
@@ -1,1 +1,7 @@
+//  route for get call GET 127.0.0.1:8000/ideaApp/api/v1/ideas
+const idea_controller = require("../controllers/idea.controller")
+
+module.exports = (app)=>{
+    app.get("/ideaApp/api/v1/ideas",idea_controller.getAllIdeas)
+}
 
