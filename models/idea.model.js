ffb88331706893b65af142777c11b417ee23e75d
agrani Kumar <158788185+Agrani-kumar-01@users.noreply.github.com>
2024-03-18 16:55:35 +0530

    Update idea.model.js

--- models/idea.model.js
+++ models/idea.model.js
@@ -1,1 +1,20 @@
-
+module.exports = {
+    1 : {
+        id : 1,
+        idea_name : "great first idea",
+        author_name : "nikhil singh",
+        idea_description : "This is the finest idea that i have ever seen"
+    },
+    2 : {
+        id : 2,
+        idea_name : "great second idea",
+        author_name : "satyam sharma",
+        idea_description : "This is the finest idea that i have ever seen"
+    },
+    3 : {
+        id : 3,
+        idea_name : "great third idea",
+        author_name : "suman singh",
+        idea_description : "This is the finest idea that i have ever seen"
+    }
+}
